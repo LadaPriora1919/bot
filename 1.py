2f800be751a58c62ae3e92d04f4462fa6614512d
LadaPriora1919 <116972446+LadaPriora1919@users.noreply.github.com>
2023-02-22 14:49:45 +0200

    Delete 1.py

--- 1.py
+++
@@ -1,6 +0,0 @@
-from binance.client import Client
-client_binance = Client('ejOiO2UMdeYxH399CzbgMlfnGxt1PhPH6uQfsrAC388W1suiLmz1pSBkD7CNiwhj', 'OZKGFKWoTZgmBzNKVipxswiL8idgUPBzUH87idZlPEK3THdNZiZA7mbyec30w8su', tld='com', testnet=False)
-
-resp = client_binance.c2c
-
-print(resp)
